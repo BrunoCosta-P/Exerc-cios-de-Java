ddcc3b22d619c9ea01cf3d71a3e4dd7648fd8833
Bruno Costa Pinto <brunocosta524@hotmail.com>
2021-11-23 11:12:07 -0300

    add fatoral

---
+++ sintaxe-variaveis-e-fluxo/src/Fatoral.java
@@ -0,0 +1,12 @@
+
+public class Fatoral {
+
+	public static void main(String[] args) {
+		int fatoral = 1;
+		for (int i = 1;i < 11; i++) {
+			fatoral *= i;
+			System.out.println("Fatoral de "+ i + " é " + fatoral);
+		}
+	}
+
+}
